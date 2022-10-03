230d9d47908cfbb2a64b0d8f5bf041e9105c5081
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-10-03 11:04:06 +0000

    Tests working. Can't find table header

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL;
-const Collection = require('./Collection.js');
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory:';
 const foodSchema = require('./food.js');
 
-let sequelize = new Sequelize(DATABASE_URL);
+let herokuOptions = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
 
-const FoodModel = foodSchema(sequelize, DataTypes);
+let sequelize = new Sequelize(DATABASE_URL, process.env === 'production' ? herokuOptions : {});
+
+let FoodModel = foodSchema(sequelize, DataTypes);
 
 module.exports = {
-  Food: new Collection(FoodModel),
+  Food: FoodModel,
   db: sequelize,
 };
